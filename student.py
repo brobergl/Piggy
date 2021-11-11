b44eda54ef5c3ffc1ebb9f7947f56054fcc7dd12
Lars Broberg <91563363+brobergl@users.noreply.github.com>
2021-11-11 20:04:49 +0000

    shake fix

--- student.py
+++ student.py
@@ -89,7 +89,7 @@ class Piggy(PiggyParent):
 
     def safe_to_dance(self):
         """ Does a 360 distance check and returns true if safe """
-        if safe_to_dance():
+        #if safe_to_dance():
     
     
     def shake(self):
