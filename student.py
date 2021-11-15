771c9ad23e91cd9621c9a1c3825f1de0a5f18795
Lars Broberg <91563363+brobergl@users.noreply.github.com>
2021-11-15 20:13:52 +0000

    distance chck v 5

--- student.py
+++ student.py
@@ -42,7 +42,7 @@ class Piggy(PiggyParent):
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit),
                 "l": ("Lars Test", self.lars)
-                }
+                "s": ("Square", self.square)}
         # loop and print the menu...
         for key in sorted(menu.keys()):
             print(key + ":" + menu[key][0])
@@ -61,8 +61,8 @@ class Piggy(PiggyParent):
       distance = self.read_distance()
       if self.read_distance() < 150 :
         self.stop()
-      while True:
         print (self.read_distance())
+   
     def square(self):
       
       for i in range(4):
